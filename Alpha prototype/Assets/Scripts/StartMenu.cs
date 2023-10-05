444c389fff458c1fc995a5390a0c72f2206697dd
N.D. Bhavana <ndbhavanastarts@gmail.com>
2023-10-05 14:12:44 -0700

    Menu

---
+++ Alpha prototype/Assets/Scripts/StartMenu.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartMenu : MonoBehaviour
+{
+   
+    public void OnStartButton() {
+        Debug.Log("start");
+    }
+    public void OnQuitButton()
+    {
+        Debug.Log("byeeeee");
+
+    }
+}
