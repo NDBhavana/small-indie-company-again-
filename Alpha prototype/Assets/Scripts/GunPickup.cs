57d78e69431fa07f3505c2b4ded032e755f770f7
hersh311 <61861759+hersh311@users.noreply.github.com>
2023-10-12 00:53:57 +0530

    Level 1

---
+++ Alpha prototype/Assets/Scripts/GunPickup.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunPickup : MonoBehaviour
+{
+    public GameObject gun;   
+    public GameObject oppwall;   
+    void Update()
+    {
+        transform.RotateAround(transform.position, transform.up, Time.deltaTime * 90f);
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+
+        Pick(collision);
+        Debug.Log("hitted");
+        Destroy(this.gameObject);
+    }
+
+    private void Pick(Collider collision)
+    {
+        //enable
+        gun.SetActive(true);
+        oppwall.SetActive(true);
+    }
+
+}
