79f9fef44df43380f24d357e64c12c2f6362ebd9
N.D. Bhavana <ndbhavanastarts@gmail.com>
2023-10-20 11:09:45 -0700

    webgl build

--- Alpha prototype/Assets/restart_fall.cs
+++ Alpha prototype/Assets/restart_fall.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Unity.SceneManagement;
+using UnityEngine.SceneManagement;
 
 public class restart_fall : MonoBehaviour
 {
