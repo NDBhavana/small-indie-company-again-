27fb986aa9523dc1c8f54162ec18c549b6c94983
N.D. Bhavana <ndbhavanastarts@gmail.com>
2023-10-19 23:45:01 -0700

    oppwall text

--- Alpha prototype/Assets/Scripts/WallController.cs
+++ Alpha prototype/Assets/Scripts/WallController.cs
@@ -1,13 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class WallController : MonoBehaviour
 {
     public Material startMaterial; // Reference to the green material.
     public Material stopMaterial; // Reference to the red material.
     private bool isGreen = false; // Indicates if the wall is currently green.
-
+    public TMP_Text txt;
     private Renderer wallRenderer; // Reference to the renderer component.
     private Collider wallCollider; // Reference to the collider attached to the wall.
 
@@ -16,7 +16,7 @@ public class WallController : MonoBehaviour
         // Get references to the renderer and collider components.
         wallRenderer = GetComponent<Renderer>();
         wallCollider = GetComponent<Collider>();
-
+        
         // Ensure that the collider is initially enabled (red state).
         if (wallCollider != null)
         {
@@ -33,7 +33,7 @@ public class WallController : MonoBehaviour
         {
             // Change the wall's material back to the red material.
             wallRenderer.material = stopMaterial;
-
+            txt.text = "STOP";
             // Enable the collider to block the player.
             if (wallCollider != null)
             {
@@ -46,7 +46,7 @@ public class WallController : MonoBehaviour
         {
             // Change the wall's material to the green material.
             wallRenderer.material = startMaterial;
-
+            txt.text = "START";
             // Disable the collider to allow the player to pass through.
             if (wallCollider != null)
             {
