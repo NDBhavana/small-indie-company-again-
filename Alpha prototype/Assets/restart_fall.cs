a6123fcf1e8e9bcdf50526542788e05ad9bc437a
N.D. Bhavana <ndbhavanastarts@gmail.com>
2023-10-20 00:00:32 -0700

    restart scene for pit

---
+++ Alpha prototype/Assets/restart_fall.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.SceneManagement;
+
+public class restart_fall : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "playerbody")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
